2976cb48e3dc865504daacf0a33eb5f1212ccabb
peterru4 <31293179+peterru4@users.noreply.github.com>
2017-10-17 17:51:44 -0400

    Update classification.py

--- additional/experiments/classification/classification.py
+++ additional/experiments/classification/classification.py
@@ -12,3 +12,26 @@ data = requests.get(url).text
 
 data = re.split('\n|, ',data)
 pairs = [i.split(' ') for i in data]
+pairs = pairs[:-1]
+
+
+times, cats = [],[]
+
+for i in pairs: 
+    times.append(pd.to_numeric(i[0]))
+    cats.append(pd.to_numeric(i[1]))
+
+df = pd.DataFrame({'Time':times, 'Category':cats})
+grouped = df.groupby('Category').sum()
+
+sns.set()
+g = sns.boxplot(x=cats,y=times)
+
+g.set_title('Experiment: Classification')
+g.set_xlabel("Category")
+g.set_ylabel("Time Spent for Corrections")
+plt.show()
+print(sums)
+
+sns.countplot(y = cats, orient = 'v')
+plt.show()
