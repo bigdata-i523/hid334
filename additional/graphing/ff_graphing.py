50000ed1fd97cb27a87accd285904f98d61d128e
peterru4 <31293179+peterru4@users.noreply.github.com>
2017-10-17 10:06:26 -0400

    Update ff_graphing.py

--- additional/graphing/ff_graphing.py
+++ additional/graphing/ff_graphing.py
@@ -13,14 +13,14 @@ import statsmodels.api as sm
 http = urllib3.PoolManager()
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-main_site = 'http://fftoday.com/stats/playerstats.php?Season=2017&GameWeek=Season&PosID=30&LeagueID='
+main_site = 'http://fftoday.com/stats/playerstats.php?Season=2017&GameWeek=Season&PosID=40&LeagueID='
 
 response = http.request('GET', main_site)
 soup = BeautifulSoup(response.data,'lxml')
 
-page_type = [i.get_text() for i in soup.select('.pageheader')][0][:-11]
+#Pull in all of the stat categories on the website 
+page_type = [i.get_text() for i in soup.select('.pageheader')][0][:-12]
 df_headers = []
-print(page_type)
 
 #Stat table headers - the ones we care about are 'G','Att','Target','FPts/G'
 fields = soup.select('b')
@@ -46,8 +46,12 @@ for player in players:
 # Find the stats
 stats = table.findAll("td", {"class" : "sort1","align":"center"})
 
+#Tight Ends needs special consideration because they don't have 
+#an 'Att' field like other positions - this is considered later as well
+if page_type != 'Tight End':
+    att_loc = df_headers.index('Att')
+
 g_loc = df_headers.index('G') 
-att_loc = df_headers.index('Att')
 rec_loc = df_headers.index('Rec')
 fpt_loc = df_headers.index('FPts')
 
@@ -57,8 +61,15 @@ fpt_loc = df_headers.index('FPts')
 # above to remove the formatting on the site
 
 jump = len(df_headers)-1
-fields = ['g', 'att', 'rec', 'fpt']
-locs = [g_loc, att_loc, rec_loc, fpt_loc] 
+
+#Ignore the 'Att' field for TEs
+if page_type !='Tight End':
+    fields = ['g', 'att', 'rec', 'fpt']
+    locs = [g_loc, att_loc, rec_loc, fpt_loc] 
+else: 
+    fields = ['g','rec', 'fpt']
+    locs = [g_loc, rec_loc, fpt_loc] 
+    
 summary = {}
 
 for i in range(0,len(fields)):
@@ -68,13 +79,16 @@ for i in range(0,len(fields)):
 summary['player']=df_players
 
 data = pd.DataFrame(summary)
-data['touches'] = [int(i)+int(j) for i,j in zip(data['att'], data['rec'])]
 
-#this is needed later because the OLS replaces the column with a constant
+if page_type !='Tight End':
+    data['touches'] = [int(i)+int(j) for i,j in zip(data['att'], data['rec'])]
+else: 
+    data['touches'] = [int(i) for i in data['rec']]
+
+#Make the graphing dataset
 data['fpt'] = data['fpt'].apply(pd.to_numeric)
 data['pos'] = page_type
 graph_data = data[['player','fpt','touches']]
-
 
 #Making the graph in Seaborn
 sns.set()
@@ -106,12 +120,21 @@ bot_resid = data.res.tail(resid_num)
 players_to_note = []
 
 allresid = [top_resid,bot_resid]
+
+# The Tight End data frame will look different than the others
+# Namely, TE data frame is one column shorter (missing 'Att')
+if page_type == 'Tight End':
+    TE_adj = 1
+else: 
+    TE_adj = 0
+    
 for i in allresid: 
     for j in i:
-        p = data[data.res == j].iloc[0,3] #Player Name
-        x = data[data.res == j].iloc[0,5] #Touches
-        y = data[data.res == j].iloc[0,1] #Points
-        r = data[data.res == j].iloc[0,7] #Residual
+
+        p = data[data.res == j].iloc[0,3-TE_adj] #Player Name
+        x = data[data.res == j].iloc[0,5-TE_adj] #Touches
+        y = data[data.res == j].iloc[0,1-TE_adj] #Points
+        r = data[data.res == j].iloc[0,7-TE_adj] #Residual
 
         players_to_note.append((p,x,y,r))
 
@@ -119,13 +142,12 @@ for i in allresid:
 for i in players_to_note: 
     plt.annotate(i[0],xy=(i[1],i[2]),xytext=(i[1],i[2]), ha = 'center', va = 'bottom')
 
-    
 #Find the highest scoring player 
 data = data.sort_values('fpt', ascending = False)
 tp = data.head(1) #tp = Top Player
-tp_name = tp.iloc[0,3]
-tp_touches = tp.iloc[0,5]
-tp_pts = tp.iloc[0,1]
+tp_name = tp.iloc[0,3-TE_adj]
+tp_touches = tp.iloc[0,5-TE_adj]
+tp_pts = tp.iloc[0,1-TE_adj]
 
 plt.annotate(tp_name,xy=(tp_touches,tp_pts),xytext=(tp_touches,tp_pts),color = 'green', ha = 'center', va = 'bottom')
 
