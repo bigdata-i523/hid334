a05799c2a9acba592b88d1fe75de21754ab0e5a1
peterru4 <31293179+peterru4@users.noreply.github.com>
2017-11-21 00:13:01 -0500

    Update project.py

--- project/project.py
+++ project/project.py
@@ -4,20 +4,32 @@ from kivy.clock import Clock
 from kivy.properties import StringProperty
 from random import *
 import time 
+from weather import Weather
+
 # In the kv file, everything to the right of the colon is pure python
 # for loading python module in kv file, use format of #:  import keyword module_name
+
+# Pull in the weather data
+weather = Weather()
+location = weather.lookup_by_location('New York, NY')
+condition = location.condition()
+forecasts = location.forecast()
+astronomy = location.astronomy()
 
 class WeatherWidget(GridLayout):
 	TimeSeconds = StringProperty('')
 	TimeMinutes = StringProperty('')
 	TimeHours = StringProperty('')
 
-	def printing_test(self):
-		return 'This is a test'
+	def current_location(self):
+		return location.title().replace('Yahoo! Weather - ','')
+
+	def current_temperature(self):
+		return condition['temp'] + '° ' +condition['text']
 
 	def update(self, dt):
 		current_time = time.localtime()
-		self.TimeHours = str(current_time.tm_hour - 12).zfill(2)
+		self.TimeHours = str(current_time.tm_hour).zfill(2)
 		self.TimeMinutes = str(current_time.tm_min).zfill(2)
 		self.TimeSeconds = str(current_time.tm_sec).zfill(2)
 
