2acd5ef640863aa43e616ebdec4b434f5c60fe3d
peterru4 <31293179+peterru4@users.noreply.github.com>
2017-11-20 23:58:37 -0500

    working clock

--- project/project.py
+++ project/project.py
@@ -1,18 +1,31 @@
-import kivy
-from time import strftime
-
 from kivy.app import App
-from kivy.uix.button import Label
-from kivy.uix.boxlayout import BoxLayout
-from kivy import clock
+from kivy.uix.gridlayout import GridLayout
+from kivy.clock import Clock
+from kivy.properties import StringProperty
+from random import *
+import time 
+# In the kv file, everything to the right of the colon is pure python
+# for loading python module in kv file, use format of #:  import keyword module_name
 
-class WeatherGrid(BoxLayout):
-    def update_time(self, nap):
-        self.root.ids.time.text = strftime('[b]%H[/b]:%M:%S')
+class WeatherWidget(GridLayout):
+	TimeSeconds = StringProperty('')
+	TimeMinutes = StringProperty('')
+	TimeHours = StringProperty('')
+
+	def printing_test(self):
+		return 'This is a test'
+
+	def update(self, dt):
+		current_time = time.localtime()
+		self.TimeHours = str(current_time.tm_hour - 12).zfill(2)
+		self.TimeMinutes = str(current_time.tm_min).zfill(2)
+		self.TimeSeconds = str(current_time.tm_sec).zfill(2)
 
 class DailyViewApp(App):
     def build(self):
-        return WeatherGrid()
+    	weather = WeatherWidget()
+    	Clock.schedule_interval(weather.update, 1)
+    	return weather
 
 if __name__ == '__main__':
     DailyViewApp().run()
