eba470b769d0693853ea5eb5d1ff9405aab34173
peterru4 <31293179+peterru4@users.noreply.github.com>
2017-11-21 15:35:30 -0500

    Update project.py

--- project/project.py
+++ project/project.py
@@ -4,7 +4,12 @@ from kivy.clock import Clock
 from kivy.properties import StringProperty
 from random import *
 import time 
+from time import strftime
+from datetime import datetime
 from weather import Weather
+import requests
+from bs4 import BeautifulSoup 
+import webbrowser
 
 # In the kv file, everything to the right of the colon is pure python
 # for loading python module in kv file, use format of #:  import keyword module_name
@@ -17,6 +22,7 @@ forecasts = location.forecast()
 astronomy = location.astronomy()
 
 class WeatherWidget(GridLayout):
+
 	TimeSeconds = StringProperty('')
 	TimeMinutes = StringProperty('')
 	TimeHours = StringProperty('')
@@ -27,17 +33,65 @@ class WeatherWidget(GridLayout):
 	def current_temperature(self):
 		return condition['temp'] + '° ' +condition['text']
 
+	def current_date(self):
+		return time.strftime('%a %b %d')
+
+	# day_num is an integer where 0 = today
+	def high_low_temp(self, day_num):
+		return forecasts[day_num].low() + 'º / ' + forecasts[day_num].high() + 'º '
+
+	def forecast_day(self, day_num):
+		return datetime.strptime(forecasts[day_num].date(), '%d %b %Y').strftime('%a')
+
+	def get_location(self):
+		return location.title().replace('Yahoo! Weather - ','')
+
+	def sunrise(self):
+		return 'Sunrise: ' + astronomy.get('sunrise')
+
+	def sunset(self):
+		return 'Sunset: ' + astronomy.get('sunset')
+
 	def update(self, dt):
 		current_time = time.localtime()
 		self.TimeHours = str(current_time.tm_hour).zfill(2)
 		self.TimeMinutes = str(current_time.tm_min).zfill(2)
 		self.TimeSeconds = str(current_time.tm_sec).zfill(2)
 
+	def pull_site(self, site):
+		url = site 
+		response = requests.get(url)
+		soup = BeautifulSoup(response.content, features = 'xml')
+		items = soup.findAll('item')
+		return items
+
+	def quote_of_the_day(self):
+		items = self.pull_site('http://feeds.feedburner.com/brainyquote/QUOTEBR')
+		return items[0].description.text + '  -' +items[0].title.text
+
+	def top_world_news_title(self, story_num):
+		items = self.pull_site('http://www.wsj.com/xml/rss/3_7085.xml')
+		return items[story_num].title.text
+
+	def top_world_news_story(self, story_num):
+		items = self.pull_site('http://www.wsj.com/xml/rss/3_7085.xml')
+		print(items[0])
+		return items[story_num].description.text
+
+	def top_business_title(self, story_num):
+		items = self.pull_site('http://www.wsj.com/xml/rss/3_7014.xml')
+		return items[story_num].title.text
+
+	def top_business_story(self, story_num):
+		items = self.pull_site('http://www.wsj.com/xml/rss/3_7014.xml')
+		print(items[0])
+		return items[story_num].description.text
+
 class DailyViewApp(App):
     def build(self):
-    	weather = WeatherWidget()
-    	Clock.schedule_interval(weather.update, 1)
-    	return weather
+    	weather_widget = WeatherWidget()
+    	Clock.schedule_interval(weather_widget.update, 1)
+    	return weather_widget
 
 if __name__ == '__main__':
     DailyViewApp().run()
