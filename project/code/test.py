2cb6f8720d0de6802ecd2d013a8bc7c81561456a
peterru4 <31293179+peterru4@users.noreply.github.com>
2017-12-06 13:33:51 -0500

    Update test.py

--- project/code/test.py
+++ project/code/test.py
@@ -1,5 +1,8 @@
 from PIL import Image
-import os
-parentDirectory = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
-image = Image.open(os.path.join(parentDirectory, 'WeatherIcons'))
+# import os
+# parentDirectory = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+
+# image = Image.open(os.path.join(parentDirectory, 'WeatherIcons', 'Code32.PNG'))
+
+image = Image.open('https:\\github.com\\bigdata-i523\\hid334\\blob\\master\\project\\code\\images\\testimage.png')
 image.show()
